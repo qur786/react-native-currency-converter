8bdd16d5c18dc8d66a98c9b6937610aa1e38f5c3
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-03 22:54:03 +0530

    feat: added amount input

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import React from "react";
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { CurrencyDropdown } from "./components/CurrencyDropdown";
 
 function App(): React.JSX.Element {
+  const [amount, setAmount] = useState("");
+
   return (
     <>
       <StatusBar />
       <SafeAreaView style={styles.container}>
         <Text style={styles.title}>Currency Converter</Text>
+        <View style={styles.amountContainer}>
+          <Text style={styles.amountLabel}>Amount</Text>
+          <TextInput
+            value={amount}
+            onChangeText={setAmount}
+            keyboardType="number-pad"
+            placeholder="Enter amount"
+            style={styles.amountInput}
+          />
+        </View>
         <View style={styles.dropdownContainer}>
           <CurrencyDropdown containerStyle={styles.currencyDropdownFrom} />
           <CurrencyDropdown containerStyle={styles.currencyDropdownTo} />
@@ -20,12 +39,12 @@ function App(): React.JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingHorizontal: 16,
   },
   dropdownContainer: {
     flexDirection: "row",
     gap: 10,
-    paddingHorizontal: 10,
-    marginVertical: 10,
+    marginVertical: 16,
   },
   currencyDropdownFrom: {
     flexShrink: 1,
@@ -40,6 +59,15 @@ const styles = StyleSheet.create({
     marginVertical: 16,
     color: "#3C40C6",
     fontFamily: "cursive",
+  },
+  amountContainer: {},
+  amountLabel: {
+    marginVertical: 12,
+    color: "#2F363F",
+  },
+  amountInput: {
+    borderWidth: 1,
+    borderRadius: 8,
   },
 });
 
